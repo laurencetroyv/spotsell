cca2b570b7e416c0a57f01f434aeba64b9112b8a
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2025-09-24 14:29:53 +0800

    ✨ feat: Enable path-based URL strategy for Flutter web
    Integrates `flutter_web_plugins` to use a path-based URL strategy, improving the URL structure for Flutter web applications. Updates dependencies in `pubspec.yaml` and `pubspec.lock` to declare `flutter_web_plugins` as a direct dependency.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:yaru/yaru.dart';
 
 import 'package:spotsell/src/ui/app.dart';
@@ -16,5 +17,6 @@ Future<void> main() async {
     }
   }
 
+  usePathUrlStrategy();
   runApp(const App());
 }

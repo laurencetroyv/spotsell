4d6315f19e388f1339edc830b61e14d16fc2f3aa
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2025-09-26 21:13:13 +0800

    ✨ feat: Add initial products screen

---
+++ lib/src/ui/feature/seller/pages/products_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:spotsell/src/data/entities/store_request.dart';
+
+class ProductsScreen extends StatelessWidget {
+  const ProductsScreen(this.store, {super.key});
+
+  final Store store;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

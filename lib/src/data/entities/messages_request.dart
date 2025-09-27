647d2394e6597bca34031f26c014b54d55f4f1bb
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2025-09-27 21:45:51 +0800

    ♻️ chore: Separate meta and sort order to its own entity class

--- lib/src/data/entities/messages_request.dart
+++ lib/src/data/entities/messages_request.dart
@@ -1,4 +1,5 @@
 import 'package:spotsell/src/data/entities/auth_request.dart';
+import 'package:spotsell/src/data/entities/meta_request.dart';
 import 'package:spotsell/src/data/entities/store_request.dart';
 
 class Conversation {
@@ -71,26 +72,6 @@ class ConversationRequest {
   Map<String, dynamic> toJson() {
     return {"store_id": storeId, "message": message};
   }
-}
-
-enum SortOrder { asc, desc }
-
-class Meta {
-  num? page = 1;
-  num? perPage = 15;
-  String? search = '';
-  bool? showAll = false;
-  String? sortBy = '';
-  SortOrder? sortOrder = SortOrder.asc;
-
-  Meta({
-    this.page,
-    this.perPage,
-    this.search,
-    this.showAll,
-    this.sortBy,
-    this.sortOrder,
-  });
 }
 
 /// Seller
